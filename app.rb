6dc63e1bf4b6319c7ac6f68bdaf878dbb1f50baf
rav <reznichenkoav@me.com>
2024-08-30 13:47:17 +0300

    Fix: refactoring2

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 class App
   def call(env)
     request = Rack::Request.new(env)
-    time_formatting = TimeFormatting.new(request).check_request
-    Rack::Response.new(time_formatting[:body], time_formatting[:status]).finish
+    @tf = TimeFormatting.new(request)
+    request.params['format'] ? show_time : response('Time format request is empty', 400)
+  end
+
+  private
+
+  def show_time
+    @tf.check_time_format
+    @tf.correct? ? response(@tf.format_time, 200) : response("Unknown time format[#{@tf.incorrect}]", 401)
+  end
+
+  def response(body, status)
+    Rack::Response.new(body, status).finish
   end
 end
--- time_format.rb
+++ time_format.rb
@@ -13,15 +13,22 @@ class TimeFormatting
     @incorrect_elements = []
   end
 
-  def check_request
-    @req.params['format'] ? check_time_format : { status: 200, body: 'Time format request is empty' }
-  end
-
   def check_time_format
     split_request = @req.params['format'].split(',')
     split_request.each do |element|
       TIME_FORMATS[element].nil? ? @incorrect_elements << element : @correct_elements << TIME_FORMATS[element]
     end
-    @incorrect_elements.empty? ? { status: 200, body: Time.now.strftime(@correct_elements.join('-')) } : { status: 401, body: "Unknown time format [#{@incorrect_elements.join(',')}]" }
+  end
+
+  def format_time
+    Time.now.strftime(@correct_elements.join('-'))
+  end
+
+  def correct?
+    @incorrect_elements.empty?
+  end
+
+  def incorrect
+    @incorrect_elements.join(', ')
   end
 end
